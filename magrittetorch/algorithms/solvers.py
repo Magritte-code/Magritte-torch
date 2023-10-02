Added mean line intensity computation. Fixed optical depth computation.

magrittetorch/algorithms/solvers.py
@@ -128,7 +128,6 @@ def solve_long_characteristics_single_direction(model: Model, raydir: torch.Tens
     #technically boundary_indices
     computed_intensity[boundary_indices] = model.get_boundary_intensity(boundary_indices, freqhelper, device)
 
-
     while (torch.any(mask_active_rays)):
         #continuously subset the tensors, reducing time needed to access the relevant subsets
         #TODO? also try subsetting computed_intensity, sum_optical_depth
@@ -139,6 +138,7 @@ def solve_long_characteristics_single_direction(model: Model, raydir: torch.Tens
         start_velocities = start_velocities[mask_active_rays,:]
 
         next_ids, distances = model.geometry.get_next(start_positions, raydir, curr_ids, distance_travelled, device, positions_device, neighbors_device, n_neighbors_device, cum_n_neighbors_device)
+        distance_increment = distances - distance_travelled
 
         distance_travelled = distances
         prev_ids = curr_ids
@@ -149,7 +149,7 @@ def solve_long_characteristics_single_direction(model: Model, raydir: torch.Tens
         #After raytracing, we now compute everything required
         doppler_shift = model.geometry.get_doppler_shift(curr_ids, start_positions, start_velocities, raydir, distances, device)
         curr_opacities, curr_emissivities = model.sources.get_total_opacity_emissivity_freqhelper(curr_ids, doppler_shift, freqhelper, device)
-        optical_depths = model.sources.get_total_optical_depth_freqhelper(curr_ids, prev_ids, start_ids, freqhelper, distances, doppler_shift, doppler_shift, curr_opacities, prev_opacities, device)
+        optical_depths = model.sources.get_total_optical_depth_freqhelper(curr_ids, prev_ids, start_ids, freqhelper, distance_increment, doppler_shift, doppler_shift, curr_opacities, prev_opacities, device)
         curr_source_function = curr_emissivities/(curr_opacities+min_opacity)
 
         #To compute the intensity contribution, we need to multiply the source function by the factors below
@@ -166,7 +166,6 @@ def solve_long_characteristics_single_direction(model: Model, raydir: torch.Tens
         ended_rays = torch.logical_not(mask_active_rays)
         computed_intensity[start_ids[ended_rays]] += model.get_boundary_intensity(curr_ids[ended_rays], freqhelper, device) * torch.exp(-sum_optical_depths[start_ids[ended_rays]])
 
-    #TODO: optionally, add NLTE stuff such as ALI
     return computed_intensity
 
 
@@ -178,6 +177,26 @@ def solve_long_characteristics_NLTE(model: Model, device: torch.device) -> torch
     model_positions = model.geometry.points.position.get(device) #dims: [parameters.npoints, 3]
     freqhelper = FrequencyEvalHelper(NLTE_freqs, model.sources.lines.lineProducingSpecies.get(), model_velocities, device)#TODO: should be result of called get_all_line_freqs
     raydirs = model.geometry.rays.direction.get(device)
+    weights = model.geometry.rays.weight.get(device)
+    total_intensity = torch.zeros_like(NLTE_freqs) #dims: [parameters.npoints, NFREQS]
+    total_integrated_line_intensity = torch.zeros((model.parameters.npoints.get(), model.sources.lines.get_total_number_lines()), dtype=Types.FrequencyInfo)
+    #add everything, with correct contribution
+    for raydir_index in range(raydirs.shape[0]):
+        print("rr:", raydir_index)
+        #Adding the results immediately, as we will otherwise run out of memory
+        total_intensity += weights[raydir_index] * solve_long_characteristics_single_direction(model, raydirs[raydir_index,:], model_positions, model_velocities, torch.arange(model.parameters.npoints.get(), device=device), freqhelper, device)
+
+    encountered_freqs: int = 0
+    # now numerically integrate the intensities of each different line# TODO: check if better option exists, without for loop
+    for lineidx in range(model.sources.lines.lineProducingSpecies.length_param.get()):
+        lspec = model.sources.lines.lineProducingSpecies[lineidx]
+        quad_weights = lspec.linequadrature.weights.get(device)
+        nfreqs: int = lspec.get_n_lines_freqs()
+        total_integrated_line_intensity[:, lineidx] = torch.sum(quad_weights[None, :] * total_intensity[:,encountered_freqs:encountered_freqs+nfreqs], dim=1)
+
+        encountered_freqs += nfreqs
+
+    return total_integrated_line_intensity
     #For initial testing, only solve for a single direction. This will be changed afterwards
     #TODO list: compute mean line intensities, ALI factors (somehow), and feed tghis into statistical equilibrium equationsn, add ng-acceleration
-    return solve_long_characteristics_single_direction(model, raydirs[0,:], model_positions, model_velocities, torch.arange(model.parameters.npoints.get(), device=device), freqhelper, device)
+    # return solve_long_characteristics_single_direction(model, raydirs[0,:], model_positions, model_velocities, torch.arange(model.parameters.npoints.get(), device=device), freqhelper, device)

magrittetorch/model/sources/lines.py
@@ -71,7 +71,7 @@ class Lines:
         # print("sum optical depth", sum_optical_depths)
         return sum_optical_depths
     
-    def get_sum_total_optical_depth_using_freqhelper(self, curr_point_indices: torch.Tensor, prev_point_indices: torch.Tensor, original_point_indices: torch.Tensor, freqhelper: FrequencyEvalHelper, curr_shift: torch.Tensor, prev_shift:torch.Tensor, curr_opacity: torch.Tensor, prev_opacity: torch.Tensor, distance_increments: torch.Tensor, device: torch.device):
+    def get_sum_total_optical_depth_using_freqhelper(self, curr_point_indices: torch.Tensor, prev_point_indices: torch.Tensor, original_point_indices: torch.Tensor, freqhelper: FrequencyEvalHelper, curr_shift: torch.Tensor, prev_shift:torch.Tensor, curr_opacity: torch.Tensor, prev_opacity: torch.Tensor, distance_increments: torch.Tensor, device: torch.device) -> torch.Tensor:
         """Computes the total line opacity and emissivity for a segment using a FrequencyEvalHelper for efficiently determining which lines need to be included in the calculation.
 
         Args:
@@ -87,7 +87,7 @@ class Lines:
             device (torch.device): Device on which to compute
 
         Returns:
-            _type_: Total optical depth for the segment. Has dimensions [NPOINTS, NFREQS]
+            torch.Tensor: Total optical depth for the segment. Has dimensions [NPOINTS, NFREQS]
         """
         nfreqs = curr_opacity.size(dim=1)
         npoints = curr_point_indices.size(dim=0)
@@ -122,7 +122,7 @@ class Lines:
             device (torch.device, optional): Device on which to compute and return the line frequencies. Defaults to torch.device("cpu").
 
         Returns:
-            torch.Tensor: All NLTE line frequencies. Has dimensions [parameters.npoints, self.get_total_number_line_frequencies()]
+            torch.Tensor: All NLTE line frequencies, ordered per line. Has dimensions [parameters.npoints, self.get_total_number_line_frequencies()]
         """
         line_frequencies = torch.empty((self.parameters.npoints.get(), self.get_total_number_line_frequencies()), dtype=Types.FrequencyInfo, device=device)
         start_freq_idx = 0