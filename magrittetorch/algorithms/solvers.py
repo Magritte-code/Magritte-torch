Added NLTE computations; seems to work Also added velocity gradient benchmark. Fixed optical depth computing in large shift regime.

magrittetorch/algorithms/solvers.py
@@ -1,4 +1,5 @@
 from magrittetorch.algorithms.raytracer import RaytracerGenerator
+from magrittetorch.algorithms.torch_algorithms import interpolate2D_linear
 from magrittetorch.model.model import Model
 from magrittetorch.model.sources.frequencyevalhelper import FrequencyEvalHelper
 from magrittetorch.utils.storagetypes import Types
@@ -116,12 +117,14 @@ def solve_long_characteristics_single_direction(model: Model, raydir: torch.Tens
     boundary_indices = curr_ids[is_boundary_point_device[curr_ids]]
     #TODO: refactor source function computation (emissivty/opacity), as some safety constant min_opacity is included
     doppler_shift = torch.ones(npoints, dtype = Types.FrequencyInfo, device=device)
+    prev_shift = torch.ones(npoints, dtype= Types.FrequencyInfo, device=device)
     prev_opacities, starting_emissivities = model.sources.get_total_opacity_emissivity_freqhelper(curr_ids, doppler_shift, freqhelper, device)
     prev_source_function = starting_emissivities/(prev_opacities+min_opacity)
 
     #already mask previous values
     prev_opacities = prev_opacities[mask_active_rays]
     prev_source_function = prev_source_function[mask_active_rays]
+    prev_shift = prev_shift[mask_active_rays]
 
     #when encountering boundary points, add their intensity contribution
     #evaluate boundary intensity
@@ -149,7 +152,7 @@ def solve_long_characteristics_single_direction(model: Model, raydir: torch.Tens
         #After raytracing, we now compute everything required
         doppler_shift = model.geometry.get_doppler_shift(curr_ids, start_positions, start_velocities, raydir, distances, device)
         curr_opacities, curr_emissivities = model.sources.get_total_opacity_emissivity_freqhelper(curr_ids, doppler_shift, freqhelper, device)
-        optical_depths = model.sources.get_total_optical_depth_freqhelper(curr_ids, prev_ids, start_ids, freqhelper, distance_increment, doppler_shift, doppler_shift, curr_opacities, prev_opacities, device)
+        optical_depths = model.sources.get_total_optical_depth_freqhelper(curr_ids, prev_ids, start_ids, freqhelper, distance_increment, doppler_shift, prev_shift, curr_opacities, prev_opacities, device)
         curr_source_function = curr_emissivities/(curr_opacities+min_opacity)
 
         #To compute the intensity contribution, we need to multiply the source function by the factors below
@@ -161,6 +164,7 @@ def solve_long_characteristics_single_direction(model: Model, raydir: torch.Tens
         #already mask the current data
         prev_source_function = curr_source_function[mask_active_rays]
         prev_opacities = curr_opacities[mask_active_rays]
+        prev_shift = doppler_shift[mask_active_rays]
 
         #and add boundary intensity to the rays which have ended, taking into account the current extincition factor e^-tau
         ended_rays = torch.logical_not(mask_active_rays)
@@ -169,9 +173,37 @@ def solve_long_characteristics_single_direction(model: Model, raydir: torch.Tens
     return computed_intensity
 
 
+def solve_long_characteristics_single_direction_all_NLTE_freqs(model: Model, raydir: torch.Tensor, device: torch.device) -> torch.Tensor:
+    """Computes the intensities in a given direction at all required frequencies for NLTE computations.
+    This is a helper function for benchmarking purposes.
+
+    Args:
+        model (Model): The model
+        raydir (torch.Tensor): The ray direction. Has dimensions [3]
+        device (torch.device): The device on which to compute
+
+    Returns:
+        torch.Tensor: The computed intensities [W/m**2/Hz/rad**2]. Has dimensions [NPOINTS, NFREQS]
+    """
+    NLTE_freqs = model.sources.lines.get_all_line_frequencies(device=device) #dims: [parameters.npoints, NFREQS]#TODO: move to function arguments; err only for imaging, this should be an argument
+    model_velocities = model.geometry.points.velocity.get(device) #dims: [parameters.npoints, 3]
+    model_positions = model.geometry.points.position.get(device) #dims: [parameters.npoints, 3]
+    freqhelper = FrequencyEvalHelper(NLTE_freqs, model.sources.lines.lineProducingSpecies.get(), model_velocities, device)#TODO: should be result of called get_all_line_freqs
+
+    return solve_long_characteristics_single_direction(model, raydir, model_positions, model_velocities, torch.arange(model.parameters.npoints.get()), freqhelper, device)
+
 
 
 def solve_long_characteristics_NLTE(model: Model, device: torch.device) -> torch.Tensor:
+    """Computes the mean line intensity J_ij at every point in the model
+
+    Args:
+        model (Model): The model
+        device (torch.device): Device on which to compute
+
+    Returns:
+        torch.Tensor: The mean line intensity. Has dimensions [parameters.npoints, model.sources.lines.get_total_number_lines()]
+    """
     NLTE_freqs = model.sources.lines.get_all_line_frequencies(device=device) #dims: [parameters.npoints, NFREQS]#TODO: move to function arguments; err only for imaging, this should be an argument
     model_velocities = model.geometry.points.velocity.get(device) #dims: [parameters.npoints, 3]
     model_positions = model.geometry.points.position.get(device) #dims: [parameters.npoints, 3]
@@ -200,3 +232,106 @@ def solve_long_characteristics_NLTE(model: Model, device: torch.device) -> torch
     #For initial testing, only solve for a single direction. This will be changed afterwards
     #TODO list: compute mean line intensities, ALI factors (somehow), and feed tghis into statistical equilibrium equationsn, add ng-acceleration
     # return solve_long_characteristics_single_direction(model, raydirs[0,:], model_positions, model_velocities, torch.arange(model.parameters.npoints.get(), device=device), freqhelper, device)
+
+
+def compute_and_set_level_populations_statistical_equilibrium(model: Model, J: torch.Tensor, device: torch.device):
+    """Computes the 
+    TODO: also include accelerated lambda iteration, just add (optional) extra functon argument
+
+    Args:
+        model (Model): The model
+        J (torch.Tensor): The (effective) mean line intensity. Has dims [parameters.npoints, parameters.nlines?]
+        device (torch.device): Device on which to set the level populations? Do we need this?
+    """
+
+    total_encountered_lines: int = 0
+    for linespecidx in range(model.parameters.nlspecs.get()):#every species is assumed to be independent
+        lspec = model.sources.lines.lineProducingSpecies[linespecidx]
+        linedata = lspec.linedata
+        nlines: int = linedata.nrad.get()
+        nlev: int = linedata.nlev.get()
+        npoints: int = model.parameters.npoints.get()
+        einsteinA = linedata.A.get(device)
+        einsteinBa = linedata.Ba.get(device)
+        einsteinBs = linedata.Bs.get(device)
+        Jrelevant = J[:, total_encountered_lines:total_encountered_lines+nlines]
+        print(Jrelevant)
+        upperidx = linedata.irad.get(device) #dims: [nlines]; all values (indices) in [0, nlev-1]
+        loweridx = linedata.jrad.get(device) #dims: [nlines]; all values (indices) in [0, nlev-1]
+        rate_upper_to_lower = torch.zeros_like(Jrelevant[0, :])
+        rate_lower_to_upper = torch.zeros_like(rate_upper_to_lower)
+        # Lambdarelevant = ... [:, also the same]
+        matrix = torch.zeros((npoints, nlev, nlev), dtype=Types.LevelPopsInfo, device=device)
+
+        rate_upper_to_lower = einsteinA[None, :] + einsteinBs[None, :] * Jrelevant#dims: [npoints, nlines]
+        rate_lower_to_upper = einsteinBa[None, :] * Jrelevant
+        # full_loweridx = torch.zeros((npoints, nlev, nlev))
+        full_upperidx = upperidx.repeat(npoints, 1)#dims: [npoints, nlines] values: [0, nlev-1]
+        full_loweridx = loweridx.repeat(npoints, 1)#dims: [npoints, nlines] values: [0, nlev-1]
+
+        print("idx", upperidx, loweridx)
+        pointidxrange = torch.arange(npoints, dtype=Types.IndexInfo, device=device)#dims: [npoints]
+
+        #TODO: check that these values are correctly put if duplicate indices are present
+        #ALSO CHECK THE indices themselves, as probably npoints is not registered correctly
+        matrix.index_put_((pointidxrange[:, None], full_loweridx, full_upperidx), rate_upper_to_lower, accumulate = True)
+        matrix.index_put_((pointidxrange[:, None], full_upperidx, full_upperidx), -rate_upper_to_lower, accumulate = True)
+        matrix.index_put_((pointidxrange[:, None], full_upperidx, full_loweridx), rate_lower_to_upper, accumulate = True)
+        matrix.index_put_((pointidxrange[:, None], full_loweridx, full_loweridx), -rate_lower_to_upper, accumulate = True)
+
+
+        #TODO: add collisional transitions somehow, after interpolating the values
+        temperature = model.thermodynamics.temperature.gas.get(device)#dims: [npoints]
+        full_abundance = model.chemistry.species.abundance.get(device)[:, :]#dims: [npoints, nspecs]
+        #for all collision partners, add their contributions to the transition matrix
+        for colpar in lspec.linedata.colpar:
+            colpar_abundance = full_abundance[:, colpar.num_col_partner.get()]#dims: [npoints]
+            #TODO: adjust the darn abundance, depending on whether we have ortho or para H2
+            
+            #and do exactly the same as for the normal level transitions
+            #dims tmp:[ntmp], Cd/Ce: [ntmp, ncol], temperature: [npoints] -> interpolated -> dims: [npoints, ncol]
+            rate_upper_to_lower = interpolate2D_linear(colpar.tmp.get(device), colpar.Cd.get(device), temperature) * colpar_abundance[:, None]#dims: [npoints, ncol]
+            rate_lower_to_upper = interpolate2D_linear(colpar.tmp.get(device), colpar.Ce.get(device), temperature) * colpar_abundance[:, None]
+            upperidx = colpar.icol.get(device)#dims:[ncol]
+            loweridx = colpar.jcol.get(device)
+            # full_loweridx = torch.zeros((npoints, nlev, nlev))
+            full_upperidx = upperidx.repeat(npoints, 1)#dims: [npoints, ncol] values: [0, nlev-1]
+            full_loweridx = loweridx.repeat(npoints, 1)#dims: [npoints, ncol] values: [0, nlev-1]
+            pointidxrange = torch.arange(npoints, dtype=Types.IndexInfo, device=device)#dims: [npoints]
+
+            #TODO: check that these values are correctly put if duplicate indices are present
+            #ALSO CHECK THE indices themselves, as probably npoints is not registered correctly
+            matrix.index_put_((pointidxrange[:, None], full_loweridx, full_upperidx), rate_upper_to_lower, accumulate = True)
+            matrix.index_put_((pointidxrange[:, None], full_upperidx, full_upperidx), -rate_upper_to_lower, accumulate = True)
+            matrix.index_put_((pointidxrange[:, None], full_upperidx, full_loweridx), rate_lower_to_upper, accumulate = True)
+            matrix.index_put_((pointidxrange[:, None], full_loweridx, full_loweridx), -rate_lower_to_upper, accumulate = True)
+
+
+
+        #and make sure that the sum of all levelpops is equal to 1
+        matrix[:, nlev-1, :] = torch.ones((npoints, nlev), dtype=Types.LevelPopsInfo, device=device)
+
+
+
+        vector = torch.zeros(npoints, nlev, dtype=Types.LevelPopsInfo, device=device)
+        vector[:, nlev-1] = full_abundance[:, lspec.linedata.num.get()]
+
+        levelpops: torch.Tensor = torch.linalg.solve(matrix, vector)
+        lspec.population.set(levelpops.to(torch.device("cpu")))#stored stuff should be mapped back to cpu?
+        print(levelpops)
+        #TODO: actually store the result somewhere
+        
+        total_encountered_lines+=nlines
+
+
+
+def compute_level_populations(model: Model, device: torch.device, max_n_iterations: int = 50) -> None:
+    #TODO: check if level pops have converged
+    #to make sure that everything is up to date, we infer
+    model.dataCollection.infer_data()
+    for i in range(max_n_iterations):
+        print("it: ", i)
+        mean_line_intensities = solve_long_characteristics_NLTE(model, device).to(device=device)
+        compute_and_set_level_populations_statistical_equilibrium(model, mean_line_intensities, device)
+        model.dataCollection.infer_data()
+

magrittetorch/model/geometry/geometry.py
@@ -298,10 +298,8 @@ class Geometry:
         Returns:
             torch.Tensor: The doppler shift. Has dimensions [NPOINTS]
         """
-        distance_to_origin = torch.sum(-origin_position*raydir, dim=1) #dims: [NPOINTS]
-        distance_diff = distance_travelled - distance_to_origin #dims: [NPOINTS]
-        # Note: projected velocity of the shell onto the ray is = distance_diff/radius * current_velocity.
-        return 1.0 + (current_velocity * distance_diff / curr_radius - torch.sum(origin_velocity * raydir, dim=1))/c.value#type: ignore
+        projected_distance_curr_to_origin = torch.sum(origin_position * raydir, dim=1) + distance_travelled
+        return 1.0 + (current_velocity * projected_distance_curr_to_origin / curr_radius - torch.sum(origin_velocity * raydir, dim=1))/c.value#type: ignore
             
 
     def get_neighbors_in_direction(self, raydir: torch.Tensor, device: torch.device) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:

magrittetorch/model/model.py
@@ -46,6 +46,7 @@ class Model:
         self.io.write(self.dataCollection)
 
     def read(self, legacy_mode: bool = False) -> None:
+        print("reading")
         self.io.read(self.dataCollection, legacy_mode)
 
     def get_boundary_intensity(self, point_indices: torch.Tensor, freqhelper: FrequencyEvalHelper, device: torch.device) -> torch.Tensor:
@@ -60,11 +61,11 @@ class Model:
             torch.Tensor: The boundary intensity. Has dimensions [NPOINTS, NFREQS]
         """
         #I need both frequency info (contained within sources) and boundary info, so this function goes in model
+        #TODO: add doppler shift, as the boundary condition slightly depends on the frequency
         return self.geometry.boundary.get_boundary_intensity(point_indices, freqhelper.original_frequencies[point_indices,:], device)
 
     # def solve_long_characteristics_NLTE_to_astropy(self, device: torch.device) -> Quantity[J/units.m**2]:
     #     solve_long_characteristics_NLTE(self, device).to_numpy(force=True)
-
 
 
     

magrittetorch/model/sources/lines.py
@@ -5,10 +5,9 @@ from magrittetorch.model.sources.lineproducingspecies import LineProducingSpecie
 from magrittetorch.model.sources.frequencyevalhelper import FrequencyEvalHelper
 from magrittetorch.utils.constants import min_freq_difference
 import torch
-from astropy import units
 
 storagedir : str = "sources/"
-
+MAX_VELOCITY_DIFFERENCE = 0.35#TODO: define param somewhere else
 
 class Lines:
     def __init__(self, params: Parameters, dataCollection : DataCollection) -> None:
@@ -96,20 +95,27 @@ class Lines:
             #check doppler shift; if too large, go compute with large variant
             #otherwise, just compute using already computed opacities
             #TODO: define param somewhere else
-            small_shift = torch.abs(curr_shift-prev_shift)<0.35*lspec.relative_line_width.get(device)[curr_point_indices]
+            small_shift = torch.abs(curr_shift-prev_shift)<MAX_VELOCITY_DIFFERENCE*lspec.relative_line_width.get(device)[curr_point_indices]
             large_shift = torch.logical_not(small_shift)
+            # print("large shift", large_shift, curr_shift - prev_shift, lspec.relative_line_width.get(device)[curr_point_indices])
             #For a small shift, we can just multiply average opacity with distance to obtain the optical depth
-            line_optical_depth = ((curr_opacity+prev_opacity)*distance_increments[:, None]*0.5)[small_shift, :]
+            line_optical_depth = ((curr_opacity+prev_opacity)*distance_increments[:, None]*0.5)#[small_shift, :]
             #For a large shift, we analytically integrate the line profile function of an averaged line width
-            non_shifted_freqs = freqhelper.duplicated_frequencies[i][original_point_indices,:]
-            mean_linewidth = lspec.sorted_linewidths.get(device)[curr_point_indices[large_shift], :]+lspec.sorted_linewidths.get(device)[prev_point_indices[large_shift], :]
-            dimensionless_curr_freq = (curr_shift[large_shift, None] * non_shifted_freqs[large_shift, :] - lspec.sorted_linefreqs.get(device)[None, :])/mean_linewidth
-            dimensionless_prev_freq = (prev_shift[large_shift, None] * non_shifted_freqs[large_shift, :] - lspec.sorted_linefreqs.get(device)[None, :])/mean_linewidth
+            non_shifted_freqs = freqhelper.duplicated_frequencies[i][original_point_indices,:]#dims: [NPOINTS, NFREQS]
+            corresponding_lines = freqhelper.corresponding_lines[i]#dims: [NFREQS]
+            #err, forgot to add factor sqrt2 to linewidths
+            mean_linewidth = 0.5*(lspec.sorted_linewidths.get(device)[curr_point_indices[large_shift], :]+lspec.sorted_linewidths.get(device)[prev_point_indices[large_shift], :])#dims: [sum(large_shift), NLINES]
+            dimensionless_curr_freq = (curr_shift[large_shift, None] * non_shifted_freqs[large_shift, :] - lspec.sorted_linefreqs.get(device)[None, corresponding_lines])/mean_linewidth[:, corresponding_lines]#dims: [sum(large_shift), NFREQS]
+            dimensionless_prev_freq = (prev_shift[large_shift, None] * non_shifted_freqs[large_shift, :] - lspec.sorted_linefreqs.get(device)[None, corresponding_lines])/mean_linewidth[:, corresponding_lines]
             dimensionless_diff_freq = dimensionless_curr_freq - dimensionless_prev_freq #+ min_freq_difference is not required, as the shift is large
-            line_opacities = lspec.line_opacities.get(device)[:, freqhelper.corresponding_lines[i]]#dims: [NPOINTS, NLINEEVAL]
-            line_optical_depth[large_shift, :] = (0.5/torch.sqrt(torch.pi*torch.ones(1, device=device)) * (line_opacities[curr_point_indices[large_shift], :]+line_opacities[prev_point_indices[large_shift], :])/mean_linewidth/dimensionless_diff_freq*(torch.erf(dimensionless_curr_freq)-torch.erf(dimensionless_prev_freq)))
+            line_opacities = lspec.line_opacities.get(device)[:, freqhelper.corresponding_lines[i]]#dims: [NPOINTS, NFREQS]
+ 
+            average_line_opacity = 0.5 * non_shifted_freqs[large_shift, :] * (line_opacities[curr_point_indices[large_shift], :] + line_opacities[prev_point_indices[large_shift], :])
+            erfterm = 0.5 * average_line_opacity / dimensionless_diff_freq * (torch.erf(dimensionless_curr_freq) - torch.erf(dimensionless_prev_freq))
+            line_optical_depth[large_shift, :] = distance_increments[large_shift, None] / mean_linewidth * erfterm
+
             expanded_freq_index = freqhelper.original_frequency_index[i].expand(npoints, nfreqs)
-            sum_optical_depths += sum_optical_depths.scatter_add(1, expanded_freq_index, line_optical_depth)#TODO: scatter sum over last dimension
+            sum_optical_depths += sum_optical_depths.scatter_add(1, expanded_freq_index, line_optical_depth)
 
         return sum_optical_depths
         