Fixed cooling rate computation

magrittetorch/model/sources/lineproducingspecies.py
@@ -313,7 +313,7 @@ class LineProducingSpecies:
         energy = self.linedata.energy.get(device)#dims = [linedata.nlev]
         temperature = self.dataCollection.get_data("gas temperature").get(device)#dims = [parameters.npoints]
         abundance = self.dataCollection.get_data("species abundance").get(device)#dims: [parameters.npoints, parameters.nspecs]
-        cooling_rate = torch.zeros(self.parameters.npoints, device=device, dtype=Types.LevelPopsInfo)
+        cooling_rate = torch.zeros(self.parameters.npoints.get(), device=device, dtype=Types.LevelPopsInfo)
         for colpar in self.linedata.colpar:
             upper_levels = colpar.icol.get(device)
             lower_levels = colpar.jcol.get(device)